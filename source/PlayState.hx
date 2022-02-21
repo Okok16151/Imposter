c5fa0090077d8732325cde1662cf10401f8508d2
Okok16151 <97395275+Okok16151@users.noreply.github.com>
2022-02-21 19:39:39 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -899,8 +899,16 @@ class PlayState extends MusicBeatState
 		iconP2.y = healthBar.y - (iconP2.height / 2);
 		iconP2.visible = !ClientPrefs.hideHud;
 		add(iconP2);
-		reloadHealthBarColors();
-
+		reloadHealthBarColors():
+		
+        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by (Keith(FNF)) "), 24);
+        creditTxt.scrollFactor.set();
+        creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        creditTxt.borderColor = FlxColor.BLACK;
+        creditTxt.borderSize = 3;
+        creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
+        add(creditTxt);
+        
 		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
 		scoreTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
